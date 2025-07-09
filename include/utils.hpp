25370e5ece4d233d75d865ab767c71e904c46668
nicholashanson <nicholashanson27@outlook.com>
2025-07-09 16:17:45 +0100

    Moved helper functions for converting bytes to intergers to utils.hpp so they can be used through out the codebase.

--- include/utils.hpp
+++ include/utils.hpp
@@ -6,7 +6,7 @@
 
 namespace ntk {
 
-	uint16_t read_uint16_be( const unsigned char* buffer, size_t offset );
+    uint16_t read_uint16_be( const unsigned char* buffer, size_t offset );
 
     uint16_t read_uint16_be( const std::vector<uint8_t>& buffer, size_t offset );
 
