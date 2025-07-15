65ab8d14b3ec7a2994079c8c91d43b72a98f753e
nicholashanson <nicholashanson27@outlook.com>
2025-07-15 17:21:32 +0100

    Moved tests for the function is_fin_ack to a seperate file for better structure and findability.

---
+++ tests/unit_tests/test_tcp/test_is_fin_ack.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+
+#include <span>
+#include <cstdint>
+
+#include <tcp.hpp>
+#include <io.hpp>
+
+#include <test_constants.hpp>
+
+TEST( UnitTest, IsFinAck_EarthCamVideo ) {
+    auto packet_data = ntk::read_packets_from_file( test::packet_data_files[ "earth_cam_video" ] );
+    auto four_tuples = ntk::get_four_tuples( packet_data );
+    auto four_tuple = *four_tuples.begin();
+    auto num_fin_acks = std::count_if( packet_data.begin(), packet_data.end(), ntk::is_fin_ack );
+
+    ASSERT_EQ( num_fin_acks, 2 );
+}
+
+TEST( UinitTest, IsFinAck_TinyCross ) {
+    auto packet_data = ntk::read_packets_from_file( test::packet_data_files[ "tiny_cross" ] );
+    auto four_tuples = ntk::get_four_tuples( packet_data );
+    auto four_tuple = *four_tuples.begin();
+    auto num_fin_acks = std::count_if( packet_data.begin(), packet_data.end(), ntk::is_fin_ack );
+
+    ASSERT_EQ( num_fin_acks, 2 );
+}
