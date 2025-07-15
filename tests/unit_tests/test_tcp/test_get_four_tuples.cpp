c92be0804da6a065a470d24639b37c66168c3ecd
nicholashanson <nicholashanson27@outlook.com>
2025-07-15 17:16:54 +0100

    Renamed tests for test-coverage verfification.

--- tests/unit_tests/test_tcp/test_get_four_tuples.cpp
+++ tests/unit_tests/test_tcp/test_get_four_tuples.cpp
@@ -9,21 +9,21 @@
 #include <test_tcp_handshake_packets.hpp>
 #include <test_constants.hpp>
 
-TEST( PacketParsingTests, GetFourTuples_TLSHandshake ) {
+TEST( UnitTest, GetFourTuples_TLSHandshake ) {
     auto packet_data = ntk::read_packets_from_file( test::packet_data_files[ "tls_handshake" ] );
     auto four_tuples = ntk::get_four_tuples( packet_data );
 
     ASSERT_EQ( four_tuples.size(), 1 ); 
 }
 
-TEST( PacketParsingTests, GetFourTuples_Checkerboard ) {
+TEST( UnitTest, GetFourTuples_Checkerboard ) {
     auto packet_data = ntk::read_packets_from_file( test::packet_data_files[ "checkerboard" ] );
     auto four_tuples = ntk::get_four_tuples( packet_data );
 
     ASSERT_EQ( four_tuples.size(), 1 ); 
 }
 
-TEST( PacketParsingTests, GetFourTuples_TinyCross ) {
+TEST( UnitTest, GetFourTuples_TinyCross ) {
     auto packet_data = ntk::read_packets_from_file( test::packet_data_files[ "tiny_cross" ] );
     auto four_tuples = ntk::get_four_tuples( packet_data );
 
