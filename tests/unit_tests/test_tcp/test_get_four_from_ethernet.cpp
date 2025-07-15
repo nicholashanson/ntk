04eb2dbbb4b5816da3aa829c291ea0eae14aecb5
nicholashanson <nicholashanson27@outlook.com>
2025-07-15 17:11:48 +0100

    Moved test for function get_four_from_ethernet to a separate file for better structure and findability.

---
+++ tests/unit_tests/test_tcp/test_get_four_from_ethernet.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include <span>
+#include <cstdint>
+
+#include <tcp.hpp>
+#include <io.hpp>
+
+#include <test_tcp_handshake_packets.hpp>
+
+TEST( UnitTest, GetFourFromEthernet ) {
+    ntk::four_tuple actual_four = ntk::get_four_from_ethernet( test_constants::tcp_syn_packet );
+    ntk::four_tuple expected_four = { 
+        .client_ip = 0xc0a80014,
+        .server_ip = 0xc0a80015,
+        .client_port = 0xac18,
+        .server_port = 0x0bb8
+    };
+
+    ASSERT_EQ( expected_four, expected_four );
+}
