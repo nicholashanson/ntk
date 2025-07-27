0668443160054bfb73a1105b1307a65abc0048a4
nicholashanson <nicholashanson27@outlook.com>
2025-07-27 12:39:15 +0100

    Added a struct template called triple and and a function called make_triple to allow the return value of slide to use structured binding.

--- include/utils.hpp
+++ include/utils.hpp
@@ -2,7 +2,9 @@
 #define UTILS_HPP
 
 #include <span>
+#include <cassert>
 #include <cstdint>
+#include <stdexcept>
 #include <concepts>
 
 namespace ntk {
@@ -83,7 +85,7 @@ namespace ntk {
 
 
     // ==============================
-    //       BIT MANIPULATION
+    //       Bit Manipulation
     // ==============================
 
     template<is_8bit_integral T>
@@ -105,6 +107,54 @@ namespace ntk {
     template<is_16bit_integral T>
     uint8_t extract_most_significant_byte( T t ) {
     	return static_cast<uint8_t>( right_shift<8>( t ) );
+    }
+
+    template<typename T>
+    struct triple {
+        const T& first;
+        const T& second;
+        const T& third;
+
+        const T& get( std::size_t i ) const {
+            switch ( i ) {
+                case 0: return first;
+                case 1: return second;
+                case 2: return third;
+                default: throw std::out_of_range( "Triple index out of range.");
+            }
+
+        }
+    };
+
+} // namespace ntk
+
+namespace std {
+
+    template<typename T>
+    struct tuple_size<ntk::triple<T>> : std::integral_constant<std::size_t,3> {};
+
+    template<typename T>
+    struct tuple_element<0,ntk::triple<T>> { using type = T; };
+    template<typename T>
+    struct tuple_element<1,ntk::triple<T>> { using type = T; };
+    template<typename T>
+    struct tuple_element<2,ntk::triple<T>> { using type = T; };
+
+    template <std::size_t I,typename T>
+    const T& get( const ntk::triple<T>& t ) {
+        if constexpr ( I == 0 ) return t.first;
+        else if constexpr ( I == 1 ) return t.second;
+        else if constexpr ( I == 2 ) return t.third;
+    }
+
+} // namespace std
+
+namespace ntk {
+
+    template<typename T>
+    triple<T> make_triple( const std::span<const T>& window ) {
+        assert( window.size() == 3 ); 
+        return triple<T>{ window[ 0 ], window[ 1 ], window[ 2 ] };
     }
 
 } // namespace ntk
