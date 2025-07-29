cecb816e55de7518d79a4e54fe458694db6a7140
nicholashanson <nicholashanson27@outlook.com>
2025-07-29 19:31:26 +0100

    Added a class https_live_stream that inherits from tls_live_stream.

--- include/https.hpp
+++ include/https.hpp
@@ -12,19 +12,19 @@ namespace ntk {
 	};
 
 	class https_live_stream : public tls_live_stream {
-		public:
-			https_live_stream( const four_tuple& four, std::string ssl_keys_log )
-				: tls_live_stream( four, ssl_keys_log ) {}
-			bool feed( const std::vector<uint8_t>& packet );
-		private:
-			std::vector<std::pair<http_request,http_response>> m_request_response;
-			incomplete_request_response m_incomplete_request_response;
-			friend class https_live_stream_friend_helper;
+        public:
+            https_live_stream( const four_tuple& four, std::string ssl_keys_log )
+            : tls_live_stream( four, ssl_keys_log ) {}
+            bool feed( const std::vector<uint8_t>& packet );
+        private:
+            std::vector<std::pair<http_request,http_response>> m_request_response;
+            incomplete_request_response m_incomplete_request_response;
+            friend class https_live_stream_friend_helper;
 	};
 
-	class https_live_stream_friend_helper {
+    class https_live_stream_friend_helper {
         public:
-            static incomplete_request_response get_incomplete_request_response( const https_live_stream& h );
+        static incomplete_request_response get_incomplete_request_response( const https_live_stream& h );
     };
 
 } // namespace ntk
