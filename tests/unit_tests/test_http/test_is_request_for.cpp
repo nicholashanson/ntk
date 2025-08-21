a1245160d5b6dad354f17219973584dc036413f1
nicholashanson <nicholashanson27@outlook.com>
2025-08-21 13:23:17 +0100

    Added test IsRequetFor_Ts and renamed test IsRequestFor to IsRequestFor_M3u8.

--- tests/unit_tests/test_http/test_is_request_for.cpp
+++ tests/unit_tests/test_http/test_is_request_for.cpp
@@ -4,7 +4,12 @@
 
 #include <test_constants.hpp>
 
-TEST( UnitTest, IsRequestFor ) {
+TEST( UnitTest, IsRequestFor_M3u8 ) {
 	auto request = ntk::get_http_request( test::http_request_for_m3u8 );
 	ASSERT_TRUE( ntk::is_request_for<ntk::file_extension::M3U8>( request ) );
+}
+
+TEST( UnitTest, IsRequestFor_Ts ) {
+	auto request = ntk::get_http_request( test::http_request_for_ts );
+	ASSERT_TRUE( ntk::is_request_for<ntk::file_extension::TS>( request ) );
 }
