f8055a74e4e82260bd2d509d8d15df67695d9586
nicholashanson <nicholashanson27@outlook.com>
2025-08-22 15:20:25 +0100

    Made changes to reflect changes return values in the http module.

--- tests/unit_tests/test_http/test_contains_htpp_header.cpp
+++ tests/unit_tests/test_http/test_contains_htpp_header.cpp
@@ -11,6 +11,6 @@
 TEST( UnitTest, ContainsHttpHeader ) {
     auto packet_data = ntk::read_packets_from_file( test::packet_data_files[ "color" ] );
     auto http_response = ntk::get_first_http_respone( packet_data );
-    auto http_headers = ntk::get_http_headers_from_payload( http_response );
+    auto http_headers = *ntk::get_http_headers_from_payload( http_response );
     ASSERT_TRUE( ntk::contains_http_header( http_headers, "Content-Length" ) );
 }
--- tests/unit_tests/test_http/test_parse_http_headers.cpp
+++ tests/unit_tests/test_http/test_parse_http_headers.cpp
@@ -6,7 +6,8 @@
 
 TEST( UnitTest, ParseHttpHeaders ) {
     auto http_payload = ntk::get_tcp_payload( test::http_get_packet );
-    auto http_sections = ntk::split_http_payload( http_payload );
-    ntk::http_headers headers = ntk::parse_http_headers( std::get<1>( http_sections ) );
+    auto maybe_split_http_message = ntk::split_http_payload( http_payload );
+    ASSERT_TRUE( maybe_split_http_message );
+    ntk::http_headers headers = ntk::parse_http_headers( ( *maybe_split_http_message ).headers );
     ASSERT_EQ( headers[ "Host" ], "192.168.0.21:3000" );
 }
