890382bad6862c85b3227e28d1522062aaa7447c
nicholashanson <nicholashanson27@outlook.com>
2025-08-22 15:22:32 +0100

    Added a function for the test get_mime_type_from_ethernet.

---
+++ tests/unit_tests/test_http/test_get_mime_type_from_ethernet.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+#include <http.hpp>
+
+#include <test_constants.hpp>
+
+TEST( UnitTest, GetMimeTypeFromEthernet_TextPlain ) {
+	auto actual_mime_type = ntk::get_mime_type_from_ethernet( test::http_response_packet );
+	ASSERT_EQ( actual_mime_type, ntk::mime_type::TEXT_PLAIN );
+}
+
+TEST( UnitTest, GetMimeTypeFromEthernet_ApplicationVndAppleMpegurl ) {
+	auto actual_mime_type = ntk::get_mime_type_from_ethernet( test::http_mpegurl_response );
+	ASSERT_EQ( actual_mime_type, ntk::mime_type::APPLICATION_VND_APPLE_MPEGURL );
+}
+
