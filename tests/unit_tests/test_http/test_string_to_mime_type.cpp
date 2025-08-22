9aab430dffc12eb5c25f31182bbfc85c545739ca
nicholashanson <nicholashanson27@outlook.com>
2025-08-22 15:21:19 +0100

    Added a test for the function string_to_mime_type.

---
+++ tests/unit_tests/test_http/test_string_to_mime_type.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+#include <http.hpp>
+
+TEST( UnitTest, StringToMimeType_TextPlain ) {
+	auto result = ntk::string_to_mime_type( "text/plain" );
+	ASSERT_TRUE( result );
+	ASSERT_EQ( *result, ntk::mime_type::TEXT_PLAIN );
+}
+
+TEST( UnitTest, StringToMimeType_ApplicationVndAppleMpegurl ) {
+	auto result = ntk::string_to_mime_type( "application/vnd.apple.mpegurl" );
+	ASSERT_TRUE( result );
+	ASSERT_EQ( *result, ntk::mime_type::TEXT_PLAIN );
+}
+
