dcf58e1d35b1a3f6479c9b4ebd7de010c597e2bd
nicholashanson <nicholashanson27@outlook.com>
2025-08-26 14:46:02 +0100

    Format chagne.

--- tests/unit_tests/test_http/test_get_mime_type_from_ethernet.cpp
+++ tests/unit_tests/test_http/test_get_mime_type_from_ethernet.cpp
@@ -13,4 +13,3 @@ TEST( UnitTest, GetMimeTypeFromEthernet_ApplicationVndAppleMpegurl ) {
 	auto actual_mime_type = ntk::get_mime_type_from_ethernet( test::http_mpegurl_response );
 	ASSERT_EQ( actual_mime_type, ntk::mime_type::APPLICATION_VND_APPLE_MPEGURL );
 }
-
