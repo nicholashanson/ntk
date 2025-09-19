fa3aebbd741f360d893bf1d95b4af5566fa32bf1
nicholashanson <nicholashanson27@outlook.com>
2025-09-19 19:20:48 +0100

    Fixed typo in filename.

--- tests/unit_tests/test_http/test_contains_http_header.cpp
+++ tests/unit_tests/test_http/test_contains_http_header.cpp
@@ -10,7 +10,9 @@
 
 TEST( UnitTest, ContainsHttpHeader ) {
     auto packet_data = ntk::read_packets_from_file( test::packet_data_files[ "color" ] );
+    ASSERT_FALSE( packet_data.empty() );
     auto http_response = ntk::get_first_http_respone( packet_data );
-    auto http_headers = *ntk::get_http_headers_from_payload( http_response );
-    ASSERT_TRUE( ntk::contains_http_header( http_headers, "Content-Length" ) );
+    auto http_headers_result = ntk::get_http_headers_from_payload( http_response );
+    ASSERT_TRUE( http_headers_result ) << http_headers_result.error() << std::endl;
+    EXPECT_TRUE( ntk::contains_http_header( http_headers_result.value(), "Content-Length" ) );
 }
