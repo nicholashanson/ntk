9940db3dd550fe41f549c27157eda47389c77652
nicholashanson <nicholashanson27@outlook.com>
2025-09-20 02:22:16 +0100

    Refactor IsRequestFor tests to use std::expected
    - Updated tests to retrieve HTTP requests using std::expected for proper error handling.
    - Added assertions to check for errors before accessing the request value.
    - Improves clarity and robustness of test cases for M3U8 and TS file requests.

--- tests/unit_tests/test_http/test_is_request_for.cpp
+++ tests/unit_tests/test_http/test_is_request_for.cpp
@@ -5,15 +5,15 @@
 #include <test_constants.hpp>
 
 TEST( UnitTest, IsRequestFor_M3u8 ) {
-	auto maybe_request = ntk::get_http_request( test::http_request_for_m3u8 );
-	ASSERT_TRUE( maybe_request );
-	auto request = *maybe_request;
+	auto request_result = ntk::get_http_request( test::http_request_for_m3u8 );
+	ASSERT_TRUE( request_result ) << request_result.error() << std::endl;
+	auto request = request_result.value();
 	ASSERT_TRUE( ntk::is_request_for<ntk::file_extension::M3U8>( request ) );
 }
 
 TEST( UnitTest, IsRequestFor_Ts ) {
-	auto maybe_request = ntk::get_http_request( test::http_request_for_ts );
-	ASSERT_TRUE( maybe_request );
-	auto request = *maybe_request;
+	auto request_result = ntk::get_http_request( test::http_request_for_ts );
+	ASSERT_TRUE( request_result ) << request_result.error() << std::endl;
+	auto request = request_result.value();
 	ASSERT_TRUE( ntk::is_request_for<ntk::file_extension::TS>( request ) );
 }
