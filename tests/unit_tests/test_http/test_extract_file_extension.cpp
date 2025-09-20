29f8bb44b60fa21c724761ea39f722602317531d
nicholashanson <nicholashanson27@outlook.com>
2025-09-20 02:14:38 +0100

    Made naming more consistent.

--- tests/unit_tests/test_http/test_extract_file_extension.cpp
+++ tests/unit_tests/test_http/test_extract_file_extension.cpp
@@ -5,9 +5,9 @@
 #include <test_constants.hpp>
 
 TEST( UnitTest, ExtractFileExtension ) {
-	auto maybe_request = ntk::get_http_request( std::span( test::http_request_for_m3u8 ) );
-	ASSERT_TRUE( maybe_request );
-	auto request = *maybe_request;
+	auto request_result = ntk::get_http_request( std::span( test::http_request_for_m3u8 ) );
+	ASSERT_TRUE( request_result ) << request_result.error() << std::endl;
+	auto request = request_result.value();
 	auto path = ntk::get_path( request.request_line.request_target );
-	ASSERT_EQ( ntk::extract_file_extension( path ), ntk::file_extension::M3U8 );
+	EXPECT_EQ( ntk::extract_file_extension( path ), ntk::file_extension::M3U8 );
 }
