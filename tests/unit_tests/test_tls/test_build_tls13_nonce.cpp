199739ca606624cc2a9ead45a63604d44258b9cd
nicholashanson <nicholashanson27@outlook.com>
2025-09-26 22:20:19 +0100

    BuildTls13Nonce: Added const correctness. GetServerHelloCipherSuite: Added checks before accessing std::expected return value to improve robustness.

--- tests/unit_tests/test_tls/test_build_tls13_nonce.cpp
+++ tests/unit_tests/test_tls/test_build_tls13_nonce.cpp
@@ -6,7 +6,7 @@ TEST( UnitTest, BuildTls13Nonce ) {
     std::vector<uint8_t> base_iv = { 0x00 ,0x01 ,0x02 ,0x03, 
                                      0x04, 0x05, 0x06, 0x07, 
                                      0x08, 0x09, 0x0a, 0x0b };
-    uint64_t seq_num = 0x0102030405060708;
+    const uint64_t seq_num = 0x0102030405060708;
     auto actual_nonce = ntk::build_tls13_nonce( base_iv, seq_num );
     std::vector<uint8_t> expected_nonce = {
         0x00, 0x01, 0x02, 0x03,
--- tests/unit_tests/test_tls/test_get_server_hello_cipher_suite.cpp
+++ tests/unit_tests/test_tls/test_get_server_hello_cipher_suite.cpp
@@ -8,7 +8,8 @@
 
 TEST( UnitTest, GetServerHelloCipherSuite ) {
 	const std::size_t cipher_suite_pos = 76;
-	auto actual_cipher_suite = *ntk::get_server_hello_cipher_suite( test_constants::tls_server_hello_record, cipher_suite_pos );
+	auto result = ntk::get_server_hello_cipher_suite( test_constants::tls_server_hello_record, cipher_suite_pos );
+	ASSERT_TRUE( result ) << result.error() << std::endl;
 	auto expected_cipher_suite = static_cast<uint16_t>( ntk::cipher_suite::TLS_AES_256_GCM_SHA384 );
-	ASSERT_EQ( actual_cipher_suite, expected_cipher_suite );
+	ASSERT_EQ( result.value(), expected_cipher_suite );
 }
