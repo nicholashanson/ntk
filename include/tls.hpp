00793b5e32f27530939505f3c9edee6da115bf44
nicholashanson <nicholashanson27@outlook.com>
2025-09-30 21:56:39 +0100

    Add TLS version and cipher suite mappings
    - Added `tls_version_names` map to convert `tls_version` enum values to strings.
    - Expanded `cipher_suite` enum with additional TLS cipher suites.
    - Added `tls_cipher_suite_names` map for converting `cipher_suite` enum values to human-readable names.
    - This update allows easier printing and debugging of TLS Client Hello messages.

--- include/tls.hpp
+++ include/tls.hpp
@@ -50,6 +50,12 @@ namespace ntk {
         tls_1_3 = 0x0304
     };
 
+    static const std::unordered_map<tls_version,std::string> tls_version_names = {
+        { tls_version::tls_1_0, "Version 1.0" },
+        { tls_version::tls_1_2, "Version 1.2" },
+        { tls_version::tls_1_3, "Version 1.3" }
+    };
+
     namespace look_up {
 
         constexpr std::array<tls_version,3/* entries in enum tls_version */> tls_versions = {
@@ -67,13 +73,43 @@ namespace ntk {
     // ===============
 
     enum class cipher_suite : uint16_t {
-        TLS_AES_128_GCM_SHA256 =  0x1301,
-        TLS_AES_256_GCM_SHA384 // 0x1302
+        TLS_RSA_WITH_AES_128_CBC_SHA                  = 0x002f,
+        TLS_RSA_WITH_AES_256_CBC_SHA                  = 0x0035,
+        TLS_RSA_WITH_AES_128_GCM_SHA256               = 0x009c,
+        TLS_RSA_WITH_AES_256_GCM_SHA384               = 0x009d,
+        TLS_AES_128_GCM_SHA256                        = 0x1301,
+        TLS_AES_256_GCM_SHA384                        = 0x1302,
+        TLS_CHACHA20_POLY1305_SHA256                  = 0x1303,
+        TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256       = 0xc02b,
+        TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA          = 0xc009,
+        TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA          = 0xc00a,
+        TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA            = 0xc013,
+        TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA            = 0xc014,
+        TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384       = 0xc02c,
+        TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256         = 0xc02f,
+        TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384         = 0xc030,
+        TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256   = 0xcca8,
+        TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256 = 0xcca9
     };
 
     static const std::unordered_map<cipher_suite,std::string> tls_cipher_suite_names = {
-        { cipher_suite::TLS_AES_128_GCM_SHA256, "TLS_AES_128_GCM_SHA256" },
-        { cipher_suite::TLS_AES_256_GCM_SHA384, "TLS_AES_256_GCM_SHA384" }
+        { cipher_suite::TLS_RSA_WITH_AES_128_CBC_SHA,                                   "TLS_RSA_WITH_AES_128_CBC_SHA" },
+        { cipher_suite::TLS_RSA_WITH_AES_256_CBC_SHA,                                   "TLS_RSA_WITH_AES_256_CBC_SHA" },
+        { cipher_suite::TLS_RSA_WITH_AES_128_GCM_SHA256,                             "TLS_RSA_WITH_AES_128_GCM_SHA256" },
+        { cipher_suite::TLS_RSA_WITH_AES_256_GCM_SHA384,                             "TLS_RSA_WITH_AES_256_GCM_SHA384" },
+        { cipher_suite::TLS_AES_128_GCM_SHA256,                                               "TLS_AES_128_GCM_SHA256" },
+        { cipher_suite::TLS_AES_256_GCM_SHA384,                                               "TLS_AES_256_GCM_SHA384" },
+        { cipher_suite::TLS_CHACHA20_POLY1305_SHA256,                                   "TLS_CHACHA20_POLY1305_SHA256" },
+        { cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,             "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256" },
+        { cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,                   "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA" },
+        { cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,                   "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA" },
+        { cipher_suite::TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,                       "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA" },
+        { cipher_suite::TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,                       "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA" },
+        { cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,             "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384" },
+        { cipher_suite::TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,                 "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256" },
+        { cipher_suite::TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,                 "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384" },
+        { cipher_suite::TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,     "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256" },
+        { cipher_suite::TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256, "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256" }
     };
 
     // ==================
