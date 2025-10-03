a92c65821ed157d29e851a007ccdec60aaed84b2
nicholashanson <nicholashanson27@outlook.com>
2025-10-03 15:47:39 +0100

    refactor(x509): clarify and standardize ASN.1 length parsing
    - Introduce named constants for offsets and minimum lengths for clarity
    - Update error messages to distinguish single-byte vs multi-byte length issues
    - Add comments explaining short-form and long-form length handling
    - Improve readability and maintainability of `parse_ans1_length`

--- src/x_509.cpp
+++ src/x_509.cpp
@@ -2,20 +2,26 @@
 
 namespace ntk {
 
+	// ===================
+    //  Parse Ans1 Length 
+    // ===================
+
 	std::expected<std::size_t,std::string> parse_ans1_length( std::span<const uint8_t> certificate_bytes ) {
-		if ( certificate_bytes.size() < 2 ) {
-			return std::unexpected( "Certificate is too short" );
+		constexpr std::size_t minimum_len = 2 /* ans1 byte + single-byte length field */; 
+		constexpr std::size_t sb = 1 /* single-byte length field offset */;
+		if ( certificate_bytes.size() < minimum_len ) {
+			return std::unexpected( "Certificate is too short for Single-Byte Length Field" );
 		}
 		std::size_t length{};
-		if ( certificate_bytes[ 1 ] < 0x80 ) {
-			return certificate_bytes[ 1 ];
+		if ( certificate_bytes[ sb ] < 0x80 /* length is <= 127 */ ) {
+			return certificate_bytes[ sb ];
 		} else {
-			std::size_t num_len_bytes = certificate_bytes[ 1 ] & 0x7f;
-			if ( certificate_bytes.size() < 2 + num_len_bytes ) {
-				return std::unexpected( "Certificate is too short for Length Field" );
+			std::size_t num_len_bytes = certificate_bytes[ sb ] & 0x7f /* clear the highest bit */;
+			if ( certificate_bytes.size() < minimum_len + num_len_bytes ) {
+				return std::unexpected( "Certificate is too short for Multi-Byte Length Field" );
 			}
 			for ( std::size_t i = 0; i < num_len_bytes; ++i ) {
-				length = ( length << 8 ) | certificate_bytes[ 2 + i ];
+				length = ( length << 8 ) | certificate_bytes[ minimum_len + i ];
 			}
 			return length;
 		}
