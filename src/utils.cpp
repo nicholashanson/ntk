c5055fd0a746d5b047a549b896c23d52b01686b2
nicholashanson <nicholashanson27@outlook.com>
2025-10-30 19:43:32 +0100

    Improved comments

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,19 +2,32 @@
 
 namespace ntk {
 
+    // ========================
+    //  Read Uint16 Big Endian
+    // ========================
+
     uint16_t read_uint16_be( const unsigned char* buffer, std::size_t offset ) {
         return ( buffer[ offset ] << 8 ) | buffer[ offset + 1 ];
     }
 
+    // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     uint16_t read_uint16_be( std::initializer_list<const char> bytes ) {
         auto it = bytes.begin();
         return ( *it << 8 ) | *( it + 1 );
     }
 
+    // ========================
+    //  Read Uint32 Big Endian
+    // ========================
+
     uint32_t read_uint32_be( const unsigned char* buffer, std::size_t offset ) {
         return ( buffer[ offset ]     << 24 ) | ( buffer[ offset + 1 ] << 16 ) |
                ( buffer[ offset + 2 ] <<  8 ) |   buffer[ offset + 3 ];
     }
+
+    // =====================
+    //  Bytes to Hex String
+    // =====================
 
     std::string bytes_to_hex_string( std::span<const uint8_t> bytes ) {
         std::ostringstream oss;
@@ -85,7 +98,7 @@ namespace ntk {
         return { 
             static_cast<uint8_t>( ( value >> 16 ) & 0xff ),
             static_cast<uint8_t>( ( value >>  8 ) & 0xff ),
-            static_cast<uint8_t>(   value &         0xff )
+            static_cast<uint8_t>(   value         & 0xff )
         };
     }
 
